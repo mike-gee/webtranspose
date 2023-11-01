69eee861cd5df792d455d61ce835dea359b0062d
Mike <michael.gee2018@gmail.com>
2023-10-31 18:38:58 -0700

    update chat docs

--- src/webtranspose/__init__.py
+++ src/webtranspose/__init__.py
@@ -4,7 +4,7 @@ __author__ = """Mike Gee"""
 __email__ = "mike@webtranspose.com"
 __version__ = "0.1.0"
 
+from .chat import *
 from .crawl import *
 from .openai import *
 from .scrape import *
-from .chat import *
--- src/webtranspose/chat.py
+++ src/webtranspose/chat.py
@@ -1,20 +1,8 @@
-import asyncio
-import json
 import logging
 import os
-import shutil
-import tempfile
-import urllib.parse
-import uuid
-import zipfile
-from datetime import datetime
-from fnmatch import fnmatch
-from typing import Dict, List, Optional
-from urllib.parse import urljoin, urlparse, urlunparse
 from time import sleep
+from typing import List
 
-import httpx
-from bs4 import BeautifulSoup
 
 from .webt_api import run_webt_api
 
@@ -30,13 +18,24 @@ class Chatbot:
         chatbot_id: str = None,
         _created: bool = False,
     ) -> None:
+        """
+        Initialize a Chatbot instance.
+
+        :param url_list: A list of URLs to crawl.
+        :param name: The name of the chatbot.
+        :param max_pages: The maximum number of pages to crawl.
+        :param api_key: The API key for accessing the Web Transpose API.
+        :param verbose: Whether to enable verbose logging.
+        :param chatbot_id: The ID of an existing chatbot.
+        :param _created: Whether the chatbot has already been created.
+        """
         self.api_key = api_key
         if self.api_key is None:
             self.api_key = os.environ.get("WEBTRANSPOSE_API_KEY")
 
         if self.api_key is None:
             raise ValueError(
-                "No Web Transpose API provided. \n\nTo Crawl on the Web Transpose API, set the WEBTRANSPOSE_API_KEY from https://webtranspose.com. Run cheaper with logging and advanced analytics."
+                "No Web Transpose API provided. \n\nTo use Chatbots, set the WEBTRANSPOSE_API_KEY from https://webtranspose.com."
             )
 
         self.url_list = url_list
@@ -50,10 +49,13 @@ class Chatbot:
             self.create()
 
     def create(self):
+        """
+        Create a chatbot.
+        """
         if not self.chatbot_id:
             self._create_chat()
             status = self.status()
-            while status['status'] != 'complete':
+            while status["status"] != "complete":
                 if self.verbose:
                     logging.info("Waiting for chat to be created...")
                 sleep(5)
@@ -63,12 +65,18 @@ class Chatbot:
             logging.info("Chat already created.")
 
     def queue_create(self):
+        """
+        Queue the creation of a chatbot.
+        """
         if not self.chatbot_id:
             self._create_chat()
         else:
             logging.info("Chat already created.")
 
     def _create_chat(self):
+        """
+        Create a chat.
+        """
         if self.verbose:
             logging.info("Creating chat...")
 
@@ -78,14 +86,17 @@ class Chatbot:
                 "max_pages": self.max_pages,
                 "url_list": self.url_list,
             }
-            out_json = run_webt_api(
-                create_json, 
-                "v1/chat/create", 
-                self.api_key
-            )
-            self.chatbot_id = out_json['chatbot_id']
-    
-    def query_database(self, query, num_records=3):
+            out_json = run_webt_api(create_json, "v1/chat/create", self.api_key)
+            self.chatbot_id = out_json["chatbot_id"]
+
+    def query_database(self, query: str, num_records: int = 3) -> list:
+        """
+        Query the database of the chatbot.
+
+        :param query: The query string.
+        :param num_records: The number of records to return.
+        :return: The query results.
+        """
         if self.verbose:
             logging.info("Querying database...")
 
@@ -97,14 +108,15 @@ class Chatbot:
             "query": query,
             "num_records": num_records,
         }
-        out = run_webt_api(
-            query_json,
-            "v1/chat/database/query",
-            self.api_key
-        )
-        return out['results']
+        out = run_webt_api(query_json, "v1/chat/database/query", self.api_key)
+        return out["results"]
 
     def status(self):
+        """
+        Get the status of the chatbot.
+
+        :return: The chatbot status.
+        """
         if self.verbose:
             logging.info("Getting chat...")
 
@@ -114,14 +126,15 @@ class Chatbot:
         get_json = {
             "chatbot_id": self.chatbot_id,
         }
-        out = run_webt_api(
-            get_json,
-            "v1/chat/get",
-            self.api_key
-        )
-        return out['chatbot']
+        out = run_webt_api(get_json, "v1/chat/get", self.api_key)
+        return out["chatbot"]
 
-    def add_urls(self, url_list):
+    def add_urls(self, url_list: list):
+        """
+        Add URLs to the chatbot.
+
+        :param url_list: A list of URLs to add.
+        """
         if self.verbose:
             logging.info("Querying database...")
 
@@ -133,13 +146,14 @@ class Chatbot:
             "max_pages": self.max_pages,
             "url_list": url_list,
         }
-        run_webt_api(
-            query_json,
-            "v1/chat/urls/add",
-            self.api_key
-        )
+        run_webt_api(query_json, "v1/chat/urls/add", self.api_key)
 
-    def delete_crawls(self, crawl_id_list):
+    def delete_crawls(self, crawl_id_list: list):
+        """
+        Delete crawls from the chatbot.
+
+        :param crawl_id_list: A list of crawl IDs to delete.
+        """
         if self.verbose:
             logging.info("Querying database...")
 
@@ -150,11 +164,4 @@ class Chatbot:
             "chatbot_id": self.chatbot_id,
             "crawl_id_list": crawl_id_list,
         }
-        run_webt_api(
-            query_json,
-            "v1/chat/crawls/delete",
-            self.api_key
-        )
-
-
-
+        run_webt_api(query_json, "v1/chat/crawls/delete", self.api_key)
